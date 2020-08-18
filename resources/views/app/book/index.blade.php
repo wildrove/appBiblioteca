7280be4e784ae2b5a38408efc0c33ad08a54b39c
Wilder V. Gaspar <wilderagner@gmail.com>
2020-08-18 16:58:30 -0300

    Update 17:00

--- resources/views/app/book/index.blade.php
+++ resources/views/app/book/index.blade.php
@@ -3,22 +3,25 @@
 
 @section('content')
 
-	<div class="row mt-5">
+	<div class="row mt-5 div-card-row">
 		@foreach($books as $book)
+		@php
+			$book_name = ucwords($book->book_name);
+		@endphp
 			<div class="col-md-3 mb-4 div-card-col zoom">
-				<div class="card div-card">
+				<div class="card div-card shadow shadow-md">
 					<div class="">
 						<img class="card-img-top img-responsive div-card-img" src="{{ url('storage/books/'.$book->book_image) }}"alt="{{$book->book_name}}">
 					</div>
-	  				<div class="card-body text-item">
-	    				<h5 class="card-title text-primary">{{$book->book_name}}</h5>
+	  				<div class="card-body d-flex flex-column text-item">
+	    				<h5 class="card-title">{{$book_name}}</h5>
 	    				<p class="card-text">
 	    					@php
 	    						$test = substr($book->description, 0, 40);
 	    					@endphp
 	    					{{$test}}.. <a href="nav-link"><br>(continue lendo...)</a>
 	    				</p>
-	    				<a href="#" class="btn btn-success btn-block">Alugar</a>
+	    				<a href="#" class=" mt-auto btn btn-success btn-block btn-card-rent">Alugar</a>
 	 				</div>
 				</div>
 			</div>
